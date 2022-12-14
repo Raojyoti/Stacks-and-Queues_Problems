939e4f8706c18b56da9d3639c045ddf461f754b1
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-14 19:56:47 +0530

    Create Queue

---
+++ UC 3-Create _Queue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UC_3_Create__Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            Console.WriteLine("Values inserted into queue : ");
+            Console.WriteLine("----------------------------");
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            Console.WriteLine("\n");
+            Console.WriteLine("Sequence of queue are: ");
+            Console.WriteLine("----------------------------");
+            linkedListQueue.Display();
+            Console.WriteLine("\n");
+        }
+    }
+}
