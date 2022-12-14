2224108407206a7fcdb61f491d8c6bd0193b41c8
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-14 19:45:02 +0530

    Create stack

---
+++ UC-1_Create  Stack/LinkedListStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_1_Create__Stack
+{
+    public class LinkedListStack
+    {
+        public Node top;
+        public LinkedListStack() 
+        {
+            this.top = null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if(this.top==null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while(temp != null)
+            {
+                Console.Write(temp.data + "->");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ UC-1_Create  Stack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UC_1_Create__Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Values push in stack are: ");
+            Console.WriteLine("----------------------------");
+            LinkedListStack linkedListStack= new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            Console.Write("\n");
+            Console.WriteLine("Sequence of stack after push values are:  ");
+            Console.WriteLine("--------------------------------------------");
+            linkedListStack.Display();
+            Console.Write("\n");
+        }
+    }
+}
